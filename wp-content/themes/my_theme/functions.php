8cece7646b240aca7356958c6a390d8e097c6bcf
Robin Mannering <robin@creative74.co.uk>
2019-10-12 16:36:42 +0200

    sass

--- wp-content/themes/my_theme/functions.php
+++ wp-content/themes/my_theme/functions.php
@@ -160,6 +160,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 function _themename_assets() {
-	wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/css/bundle.css', array(), '1.0.0', 'all' );
+	wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/css/style.css', array(), '1.0.0', 'all' );
 }
 add_action('wp_enqueue_scripts', '_themename_assets');
