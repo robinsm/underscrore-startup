d0cb114033af743436efb471bfb8d11d2459694d
Robin Mannering <robin@creative74.co.uk>
2019-10-18 11:21:25 +0200

    Added partners section

--- wp-content/themes/my_theme/template-parts/flexible-content/flexible-content.php
+++ wp-content/themes/my_theme/template-parts/flexible-content/flexible-content.php
@@ -5,18 +5,10 @@ if( have_rows('flexible_content_page') ):
 	    while ( have_rows('flexible_content_page') ) : the_row();
 
 			// Load flexible modules here
-			require('intro-section.php');
-			require('services-section.php');
-			require('features-section.php');
-
-			/*
-			require('small-offer-text.php');
-			require('three-key-points-with-image.php');
-			require('club-carousel.php');
-			require('features-grid.php');
-			require('two-testimonials.php');
-			require('price-table-carousel.php');
-			require('four-other-benefits.php');*/
+			//require('intro-section.php');
+			//require('services-section.php');
+			//require('features-section.php');
+			require('partners-section.php');
 
 
 	    endwhile;
