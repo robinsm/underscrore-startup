c5155b1088b51e5def01b07a5cfcc206b75c1159
Robin Mannering <robin@creative74.co.uk>
2019-10-20 17:08:52 +0200

    building changes

--- wp-content/themes/my_theme/gulpfile.babel.js
+++ wp-content/themes/my_theme/gulpfile.babel.js
@@ -1,7 +1,7 @@
 /*
  * https://css-tricks.com/gulp-for-wordpress-initial-setup/
  */
-import { src, dest, watch } from 'gulp';
+import { src, dest, watch, series, parallel } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
@@ -10,6 +10,8 @@ import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'autoprefixer';
 import imagemin from 'gulp-imagemin';
+import del from 'del';
+
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
@@ -22,13 +24,24 @@ export const styles = () => {
 		.pipe(dest('dist/css'));
 };
 
-export const watchForChanges = () => {
-	watch('src/scss/**/*.scss', styles);
-	watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', images);
-};
-
 export const images = () => {
 	return src('src/images/**/*.{jpg,jpeg,png,svg,gif}')
 		.pipe(gulpif(PRODUCTION, imagemin()))
 		.pipe(dest('dist/images'));
 };
+
+export const copy = () => {
+	return src(['src/**/*', '!src/{images,js,scss}', '!src/{images,js,scss}/**/*']).pipe(dest('dist'));
+};
+
+export const clean = () => del(['dist']);
+
+export const watchForChanges = () => {
+	watch('src/scss/**/*.scss', styles);
+	watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', images);
+	watch(['src/**/*', '!src/{images,js,scss}', '!src/{images,js,scss}/**/*'], copy);
+};
+
+export const dev = series(clean, parallel(styles, images, copy), watchForChanges);
+export const build = series(clean, parallel(styles, images, copy));
+export default dev;
