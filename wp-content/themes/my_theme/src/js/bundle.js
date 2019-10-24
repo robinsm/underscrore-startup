9a7ee3d800095cdfc02af792c8d07976fdd44af1
Robin Mannering <robin@creative74.co.uk>
2019-10-24 18:14:17 +0200

    added javascript compilation

---
+++ wp-content/themes/my_theme/src/js/bundle.js
@@ -0,0 +1,2 @@
+import './retina-1.3.0/retina.js';
+console.log('bundle.js loading');
--- wp-content/themes/my_theme/src/js/retina-1.3.0/retina.js
+++ wp-content/themes/my_theme/src/js/retina-1.3.0/retina.js
@@ -7,176 +7,179 @@
  * Retina.js is an open source script that makes it easy to serve
  * high-resolution images to devices with retina displays.
  */
+console.log('retina.js loading');
 
 (function() {
-    var root = (typeof exports === 'undefined' ? window : exports);
-    var config = {
-        // An option to choose a suffix for 2x images
-        retinaImageSuffix : '@2x',
+	var root = typeof exports === 'undefined' ? window : exports;
+	var config = {
+		// An option to choose a suffix for 2x images
+		retinaImageSuffix: '@2x',
 
-        // Ensure Content-Type is an image before trying to load @2x image
-        // https://github.com/imulus/retinajs/pull/45)
-        check_mime_type: true,
+		// Ensure Content-Type is an image before trying to load @2x image
+		// https://github.com/imulus/retinajs/pull/45)
+		check_mime_type: true,
 
-        // Resize high-resolution images to original image's pixel dimensions
-        // https://github.com/imulus/retinajs/issues/8
-        force_original_dimensions: true
-    };
+		// Resize high-resolution images to original image's pixel dimensions
+		// https://github.com/imulus/retinajs/issues/8
+		force_original_dimensions: true,
+	};
 
-    function Retina() {}
+	function Retina() {}
 
-    root.Retina = Retina;
+	root.Retina = Retina;
 
-    Retina.configure = function(options) {
-        if (options === null) {
-            options = {};
-        }
+	Retina.configure = function(options) {
+		if (options === null) {
+			options = {};
+		}
 
-        for (var prop in options) {
-            if (options.hasOwnProperty(prop)) {
-                config[prop] = options[prop];
-            }
-        }
-    };
+		for (var prop in options) {
+			if (options.hasOwnProperty(prop)) {
+				config[prop] = options[prop];
+			}
+		}
+	};
 
-    Retina.init = function(context) {
-        if (context === null) {
-            context = root;
-        }
+	Retina.init = function(context) {
+		if (context === null) {
+			context = root;
+		}
 
-        var existing_onload = context.onload || function(){};
+		var existing_onload = context.onload || function() {};
 
-        context.onload = function() {
-            var images = document.getElementsByTagName('img'), retinaImages = [], i, image;
-            for (i = 0; i < images.length; i += 1) {
-                image = images[i];
-                if (!!!image.getAttributeNode('data-no-retina')) {
-                    retinaImages.push(new RetinaImage(image));
-                }
-            }
-            existing_onload();
-        };
-    };
+		context.onload = function() {
+			var images = document.getElementsByTagName('img'),
+				retinaImages = [],
+				i,
+				image;
+			for (i = 0; i < images.length; i += 1) {
+				image = images[i];
+				if (!!!image.getAttributeNode('data-no-retina')) {
+					retinaImages.push(new RetinaImage(image));
+				}
+			}
+			existing_onload();
+		};
+	};
 
-    Retina.isRetina = function(){
-        var mediaQuery = '(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)';
+	Retina.isRetina = function() {
+		var mediaQuery =
+			'(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)';
 
-        if (root.devicePixelRatio > 1) {
-            return true;
-        }
+		if (root.devicePixelRatio > 1) {
+			return true;
+		}
 
-        if (root.matchMedia && root.matchMedia(mediaQuery).matches) {
-            return true;
-        }
+		if (root.matchMedia && root.matchMedia(mediaQuery).matches) {
+			return true;
+		}
 
-        return false;
-    };
+		return false;
+	};
 
+	var regexMatch = /\.\w+$/;
+	function suffixReplace(match) {
+		return config.retinaImageSuffix + match;
+	}
 
-    var regexMatch = /\.\w+$/;
-    function suffixReplace (match) {
-        return config.retinaImageSuffix + match;
-    }
+	function RetinaImagePath(path, at_2x_path) {
+		this.path = path || '';
+		if (typeof at_2x_path !== 'undefined' && at_2x_path !== null) {
+			this.at_2x_path = at_2x_path;
+			this.perform_check = false;
+		} else {
+			if (undefined !== document.createElement) {
+				var locationObject = document.createElement('a');
+				locationObject.href = this.path;
+				locationObject.pathname = locationObject.pathname.replace(regexMatch, suffixReplace);
+				this.at_2x_path = locationObject.href;
+			} else {
+				var parts = this.path.split('?');
+				parts[0] = parts[0].replace(regexMatch, suffixReplace);
+				this.at_2x_path = parts.join('?');
+			}
+			this.perform_check = true;
+		}
+	}
 
-    function RetinaImagePath(path, at_2x_path) {
-        this.path = path || '';
-        if (typeof at_2x_path !== 'undefined' && at_2x_path !== null) {
-            this.at_2x_path = at_2x_path;
-            this.perform_check = false;
-        } else {
-            if (undefined !== document.createElement) {
-                var locationObject = document.createElement('a');
-                locationObject.href = this.path;
-                locationObject.pathname = locationObject.pathname.replace(regexMatch, suffixReplace);
-                this.at_2x_path = locationObject.href;
-            } else {
-                var parts = this.path.split('?');
-                parts[0] = parts[0].replace(regexMatch, suffixReplace);
-                this.at_2x_path = parts.join('?');
-            }
-            this.perform_check = true;
-        }
-    }
+	root.RetinaImagePath = RetinaImagePath;
 
-    root.RetinaImagePath = RetinaImagePath;
+	RetinaImagePath.confirmed_paths = [];
 
-    RetinaImagePath.confirmed_paths = [];
+	RetinaImagePath.prototype.is_external = function() {
+		return !!(this.path.match(/^https?\:/i) && !this.path.match('//' + document.domain));
+	};
 
-    RetinaImagePath.prototype.is_external = function() {
-        return !!(this.path.match(/^https?\:/i) && !this.path.match('//' + document.domain) );
-    };
+	RetinaImagePath.prototype.check_2x_variant = function(callback) {
+		var http,
+			that = this;
+		if (this.is_external()) {
+			return callback(false);
+		} else if (!this.perform_check && typeof this.at_2x_path !== 'undefined' && this.at_2x_path !== null) {
+			return callback(true);
+		} else if (this.at_2x_path in RetinaImagePath.confirmed_paths) {
+			return callback(true);
+		} else {
+			http = new XMLHttpRequest();
+			http.open('HEAD', this.at_2x_path);
+			http.onreadystatechange = function() {
+				if (http.readyState !== 4) {
+					return callback(false);
+				}
 
-    RetinaImagePath.prototype.check_2x_variant = function(callback) {
-        var http, that = this;
-        if (this.is_external()) {
-            return callback(false);
-        } else if (!this.perform_check && typeof this.at_2x_path !== 'undefined' && this.at_2x_path !== null) {
-            return callback(true);
-        } else if (this.at_2x_path in RetinaImagePath.confirmed_paths) {
-            return callback(true);
-        } else {
-            http = new XMLHttpRequest();
-            http.open('HEAD', this.at_2x_path);
-            http.onreadystatechange = function() {
-                if (http.readyState !== 4) {
-                    return callback(false);
-                }
+				if (http.status >= 200 && http.status <= 399) {
+					if (config.check_mime_type) {
+						var type = http.getResponseHeader('Content-Type');
+						if (type === null || !type.match(/^image/i)) {
+							return callback(false);
+						}
+					}
 
-                if (http.status >= 200 && http.status <= 399) {
-                    if (config.check_mime_type) {
-                        var type = http.getResponseHeader('Content-Type');
-                        if (type === null || !type.match(/^image/i)) {
-                            return callback(false);
-                        }
-                    }
+					RetinaImagePath.confirmed_paths.push(that.at_2x_path);
+					return callback(true);
+				} else {
+					return callback(false);
+				}
+			};
+			http.send();
+		}
+	};
 
-                    RetinaImagePath.confirmed_paths.push(that.at_2x_path);
-                    return callback(true);
-                } else {
-                    return callback(false);
-                }
-            };
-            http.send();
-        }
-    };
+	function RetinaImage(el) {
+		this.el = el;
+		this.path = new RetinaImagePath(this.el.getAttribute('src'), this.el.getAttribute('data-at2x'));
+		var that = this;
+		this.path.check_2x_variant(function(hasVariant) {
+			if (hasVariant) {
+				that.swap();
+			}
+		});
+	}
 
+	root.RetinaImage = RetinaImage;
 
-    function RetinaImage(el) {
-        this.el = el;
-        this.path = new RetinaImagePath(this.el.getAttribute('src'), this.el.getAttribute('data-at2x'));
-        var that = this;
-        this.path.check_2x_variant(function(hasVariant) {
-            if (hasVariant) {
-                that.swap();
-            }
-        });
-    }
+	RetinaImage.prototype.swap = function(path) {
+		if (typeof path === 'undefined') {
+			path = this.path.at_2x_path;
+		}
 
-    root.RetinaImage = RetinaImage;
+		var that = this;
+		function load() {
+			if (!that.el.complete) {
+				setTimeout(load, 5);
+			} else {
+				if (config.force_original_dimensions) {
+					that.el.setAttribute('width', that.el.offsetWidth);
+					that.el.setAttribute('height', that.el.offsetHeight);
+				}
 
-    RetinaImage.prototype.swap = function(path) {
-        if (typeof path === 'undefined') {
-            path = this.path.at_2x_path;
-        }
+				that.el.setAttribute('src', path);
+			}
+		}
+		load();
+	};
 
-        var that = this;
-        function load() {
-            if (! that.el.complete) {
-                setTimeout(load, 5);
-            } else {
-                if (config.force_original_dimensions) {
-                    that.el.setAttribute('width', that.el.offsetWidth);
-                    that.el.setAttribute('height', that.el.offsetHeight);
-                }
-
-                that.el.setAttribute('src', path);
-            }
-        }
-        load();
-    };
-
-
-    if (Retina.isRetina()) {
-        Retina.init(root);
-    }
+	if (Retina.isRetina()) {
+		Retina.init(root);
+	}
 })();
