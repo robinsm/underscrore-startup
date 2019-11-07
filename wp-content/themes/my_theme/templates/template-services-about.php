d09db3807ecc353a7aa598b85e825569c8bbf722
Robin Mannering <robin@creative74.co.uk>
2019-11-07 17:44:59 +0100

    New page and styling

---
+++ wp-content/themes/my_theme/templates/template-services-about.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: About Template
+ */
+	//require(__DIR__."/custom_attributes.php");
+	get_header(); 
+ ?>
+
+<?php 
+	require( get_template_directory()  . '/template-parts/flexible-content/flexible-content-about-page.php');
+?>
+
+<?php get_footer(); ?>
