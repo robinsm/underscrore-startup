972cc0b82172f0a5741e0d9e5a52f3d185b87e66
Robin Mannering <robin@creative74.co.uk>
2019-11-27 16:53:34 +0100

    News Page

---
+++ wp-content/themes/my_theme/templates/template-news.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: News Template
+ */
+	//require(__DIR__."/custom_attributes.php");
+	get_header(); 
+ ?>
+
+<?php 
+	require( get_template_directory()  . '/template-parts/flexible-content/flexible-content-news-page.php');
+?>
+
+<?php get_footer(); ?>
