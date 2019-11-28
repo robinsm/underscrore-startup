fb2ff46cfa32f30c9e02cc31b397e57ae1894e39
Robin Mannering <robin@creative74.co.uk>
2019-11-28 15:58:00 +0100

    Added videos and updated plugins

--- wp-content/themes/my_theme/template-parts/flexible-content/shared/hero-section.php
+++ wp-content/themes/my_theme/template-parts/flexible-content/shared/hero-section.php
@@ -15,15 +15,32 @@ if( get_row_layout() == 'hero_section' ):
                 $illustration_large = $illustration['sizes'][ $illustration_size ];
             }
             
+            $illustration_video = get_sub_field('illustration_video');
+            if ($illustration_video) {
+                $illustration_video_url = $illustration_video['url'];
+            }
             $title = get_sub_field('title');
             $subtitle = get_sub_field('subtitle');
 
             $reduce_padding = get_sub_field('reduce_padding');
+
         ?>
 
     <div class="wide-container <?php echo $reduce_padding ? 'reduce-padding' : '' ?>">
         <div class="illustration">
+            <?php if (!empty($illustration_video_url)): ?>
+            <video autoplay loop>
+                <source src="<?php echo $illustration_video_url;?>" type="video/mp4">
+                <!-- <source src="movie.ogg" type="video/ogg"> -->
+                Your browser does not support the video tag.
+            </video>
+            <?php else: 
+                if (!empty($illustration_url)):    
+            ?>
             <img src="<?php echo $illustration_url;?>" alt="<?php echo $illustration_alt;?>" />
+            <?php
+                endif;
+            endif; ?>
         </div>
 
         <div class="title_and_subtitle">
