d9d22dbaf7c32d136070191db5bce508cfd4d145
Robin Mannering <robin@creative74.co.uk>
2019-12-05 11:17:26 +0100

    Optimisations to get double AA grade on gtmetrix

--- wp-content/themes/my_theme/functions.php
+++ wp-content/themes/my_theme/functions.php
@@ -123,13 +123,17 @@ add_action( 'widgets_init', 'my_theme_widgets_init' );
 function my_theme_scripts() {
 	wp_enqueue_style( 'my_theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'my_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// RM : Disabled 04 Dec 2019.  Provided by _unserscores theme but don't think we need it.
+	// wp_enqueue_script( 'my_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'my_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// RM : Disabled 04 Dec 2019.  Provided by _unserscores theme but don't think we need it.
+	// wp_enqueue_script( 'my_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	// RM : Disabled 04 Dec 2019.  Provided by _unserscores theme but don't think we need it.
+	/*
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
-	}
+	}*/
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
 
