8cc668266c8d93d84fd87d871f3bf5c9e3c8980f
Robin Mannering <robin@creative74.co.uk>
2020-01-13 09:47:07 +0100

    Refactoring

--- wp-content/themes/my_theme/template-parts/flexible-content/flexible-content-about-page.php
+++ wp-content/themes/my_theme/template-parts/flexible-content/flexible-content-about-page.php
@@ -1,12 +1,13 @@
 <?php
 
+requireHeroSectionIfExists();
+
 if ( have_rows( 'flexible_content_about_page' ) ) :
 
 	while ( have_rows( 'flexible_content_about_page' ) ) :
 		the_row();
 
 		// Load flexible modules here
-		require 'shared/hero-section.php';
 		require 'about/intro-section.php';
 		require 'about/about-blurbs-section.php';
 
--- wp-content/themes/my_theme/template-parts/flexible-content/flexible-content-services-page.php
+++ wp-content/themes/my_theme/template-parts/flexible-content/flexible-content-services-page.php
@@ -1,12 +1,13 @@
 <?php
 
+requireHeroSectionIfExists();
+
 if ( have_rows( 'flexible_content_services_page' ) ) :
 
 	while ( have_rows( 'flexible_content_services_page' ) ) :
 		the_row();
 
 		// Load flexible modules here
-		require 'shared/hero-section.php';
 		require 'services/services-section.php';
 		require 'services/single-services-section.php';
 
